8e645998998d8b7dbef4efddaabcba6a22e3db9d
Stefan Rishøj Hansen <stef6635@edu.easj.dk>
2016-11-18 10:23:16 +0100

    Add project files.

---
+++ ClassDemoTestConsole/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassDemoTestConsole
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
