143b6a0448a77ba32bfb8907aa2d67928168eb37
Stefan Rishøj Hansen <stef6635@edu.easj.dk>
2016-11-18 11:06:35 +0100

    Implementeret test-metoder

---
+++ ClassDemoTestConsole/Test/Dummy.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassDemoTestConsole.Test
+{
+    public class Dummy
+    {
+        private int somenumber;
+
+        public int Somenumber
+        {
+            get { return somenumber; }
+            set
+            {
+                if (value < 1 || 4 < value)
+                {
+                    throw new ArgumentException("somenumber must be in range 1-4");
+                }
+                somenumber = value;
+            }
+        }
+        private string sometext;
+
+        public string Sometext
+        {
+            get { return sometext; }
+            set
+            {
+                if(string.IsNullOrWhiteSpace(value))
+                    {
+                    throw new ArgumentException("Sometext must be empty or null");
+                }
+                sometext = value;
+            }
+        }
+    }
+}
---
+++ UnitTestProject1/UnitTest1.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using ClassDemoTestConsole.Test;
+
+namespace UnitTestProject1
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            Dummy dum = new Dummy();
+            try
+            {
+                dum.Somenumber = 0;
+                Assert.Fail();
+            }
+            catch (ArgumentException ae)
+            {
+
+                // ok
+            }
+        }
+
+        [TestMethod]
+        public void TestMethod2()
+        {
+            Dummy dum = new Dummy();
+
+
+            dum.Somenumber = 1;
+            Assert.AreEqual(1, dum.Somenumber);
+        }
+
+        [TestMethod]
+        public void TestMethod3()
+        {
+            Dummy dum = new Dummy();
+            try
+            {
+                dum.Somenumber = 4;
+                Assert.AreEqual(4, dum.Somenumber);
+            }
+            catch (ArgumentException ae)
+            {
+
+                // ok
+            }
+        }
+        [TestMethod]
+        public void TestMethod4()
+        {
+            Dummy dum = new Dummy();
+            try
+            {
+                dum.Somenumber = 5;
+                Assert.AreEqual(5, dum.Somenumber);
+             }
+            catch(ArgumentException ae)
+            {
+
+            }
+            }
+        }
+}
